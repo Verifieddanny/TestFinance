ef9312621197c1394d5f4d1786ae753fbc801087
your_username <dannyclassic56@gmail.com>
2023-06-29 14:51:59 +0100

    name fix

--- src/Pages/dashboardComponents/Withdraw.jsx
+++ src/Pages/dashboardComponents/Withdraw.jsx
@@ -57,7 +57,7 @@ const Withdraw = ({ theme, setNavon, setTheme }) => {
                 <Link to="/" className="flex ml-2 md:mr-24">
                   <img src={logo} className="h-8 mr-3" alt="FlowBite Logo" />
                   <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                    Finance
+                    Heaviside Finance
                   </span>
                 </Link>
               </div>
