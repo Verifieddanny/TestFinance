e86b7d1d69de8543e91a02c30dce045c78573e05
Daniel Nwachukwu <103733496+Verifieddanny@users.noreply.github.com>
2023-07-02 11:13:10 +0100

    Update Footer.jsx

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -27,6 +27,11 @@ export const FooterComponent = () => {
         <ul className="ftChild ul text-left">
           <li className="text-2xl md:text-3xl font-bold">Get in Touch</li>
           <li>
+            <a href="https://www.google.com/maps/place/199+Westminster+Bridge+Rd,+London+SE1+7FP,+UK/@51.5004552,-0.1182397,17z/data=!3m1!4b1!4m6!3m5!1s0x487604b8bfec2215:0xa0422b413e3f2153!8m2!3d51.5004519!4d-0.1156648!16s%2Fg%2F11csdlgwqf?entry=ttu">
+              Location: 199 Westminster Bridge Rd, London SE1 7FP, United Kingdom
+            </a>
+          </li>
+          <li>
             <a href="https://www.google.com/maps/place/300-600+E+Rio+Salado+Pkwy,+Tempe,+AZ+85281,+USA/@33.4304878,-111.9347183,17z/data=!3m1!4b1!4m6!3m5!1s0x872b0920fe24e517:0xfced217c7c830a48!8m2!3d33.4299188!4d-111.9317553!16s%2Fg%2F11c53mzwh7?entry=ttu">
               Location: 300-600 E Rio Salado Pkwy, Tempe, AZ 85281
             </a>
